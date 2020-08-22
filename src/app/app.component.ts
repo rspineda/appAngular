c2036cdd506a82ac5e1721380c81412c33aaba42
Ronald <ronystivens@hotmail.com>
2020-08-23 00:32:30 +0200

    rendering objets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string;
+  age: number;
+  isSingle: boolean;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +15,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '¡Que calor hace en Madrid!';
   name = 'Ronald';
-  cities = ['Ribadesella', 'Gijon', 'Santander', 'Llanes'];
+  cities: string[] = ['Ribadesella', 'Gijon', 'Santander', 'Llanes'];
   cityName = '';
+
+  users: User[] = [
+    {
+      name: 'Ronald',
+      age: 32 ,
+      isSingle: false,
+      avatar: 'assets/images/img1.jpg'
+    },
+    {
+      name: 'Stivens',
+      age: 29 ,
+      isSingle: true,
+      avatar: 'assets/images/img2.jpg'
+    },
+    {
+      name: 'Pelusa',
+      age: 11 ,
+      isSingle: true,
+      avatar: 'assets/images/img3.jpg'
+    }
+  ];
+
 
   changeTitle(){
     this.title = '¡Que venga el Otoño ya!';
